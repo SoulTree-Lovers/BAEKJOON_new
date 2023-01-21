026b858467f74f29ad192c0fd87151c3bc885937
강승민 <rkdtmdals999@naver.com>
2023-01-21 14:02:08 +0900

    Update 2023.01.21

---
+++ Class02/2839.py
@@ -0,0 +1,31 @@
+# 설탕 배달
+
+n = int(input())
+
+five_sugar = n // 5
+three_sugar = 0
+
+total = n
+
+
+
+while 1:
+    total = n
+    total -= five_sugar * 5 + three_sugar * 3
+    
+    if total < 3:
+        if total == 0:
+            print(five_sugar + three_sugar)
+            break
+
+        elif total < 0:
+            print(-1)
+            break
+        
+        if five_sugar > 0:
+            five_sugar -= 1
+        three_sugar += 1
+
+    else:
+    
+        three_sugar += 1
