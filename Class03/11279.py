16bfdcd2155981d48f91492db5f149d60ec16653
강승민 <rkdtmdals999@naver.com>
2023-02-12 14:24:25 +0900

    Update 2023.02.12

---
+++ Class03/11279.py
@@ -0,0 +1,25 @@
+# 최대 힙
+import sys
+import heapq
+input = sys.stdin.readline
+
+n = int(input())
+heap = []
+
+output = []
+
+for _ in range(n):
+    num = int(input())
+    
+    if num != 0:
+        heapq.heappush(heap, -num)
+    else:
+        try:
+            output.append(-heapq.heappop(heap))
+        except:
+            output.append(0)
+
+
+# print("----------------------------------------------------------------")
+for i in output:
+    print(i)
