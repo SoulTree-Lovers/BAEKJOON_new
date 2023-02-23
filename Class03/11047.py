d9e99304958bf18ab966fca64511f3afb2dfdebf
강승민 <rkdtmdals999@naver.com>
2023-02-23 19:08:18 +0900

    Update 2023.02.23

---
+++ Class03/11047.py
@@ -0,0 +1,28 @@
+# 동전 0
+
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+
+coin_list = []
+
+for i in range(n):
+    coin_list.append(int(input()))
+
+count = 0
+index = n-1
+
+while k > 0:
+    temp = k // coin_list[index]
+    k -= temp * coin_list[index]
+    count += temp
+    index -= 1
+
+    # if coin_list[index] <= k:
+    #     k -= k // coin_list[index] 
+    #     count += 1
+    # else:
+    #     index -= 1
+
+print(count)
