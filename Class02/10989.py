f47a9b785acfe010f0a7a46c1c1ef280b17769e3
강승민 <rkdtmdals999@naver.com>
2023-03-05 20:25:06 +0900

    Update 2023.03.05

---
+++ Class02/10989.py
@@ -0,0 +1,18 @@
+# 수 정렬하기 3
+import sys
+input = sys.stdin.readline
+
+
+n = int(input())
+
+check_list = [0] * 10001
+
+for i in range(n):
+    input_num = int(input())
+    
+    check_list[input_num] = check_list[input_num] + 1
+    
+for i in range(10001):
+    if check_list[i] != 0:
+        for j in range(check_list[i]):
+            print(i)
